af38c5f1ed685332e933af53ed0e7005e5e54274
nsofianos <nsofianos@hotmail.com>
2022-03-08 15:58:14 -0800

    more small css tweaks to button

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -4,6 +4,7 @@ import { dividerValues } from "../data/multiplierData";
 function Divider(props) {
   return (
     <ButtonGroup
+      buttonClass={"divider-button"}
       className="divider-button-group"
       onClickHandler={props.onMultiplierChange}
       buttons={dividerValues}
